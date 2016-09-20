682f9f1b361790943ce3546f08c7097a93c7772f
MikeH <mike123.henry@gmail.com>
2016-09-20 17:13:23 -0500

    initial commit with express server running and servering up root route

---
+++ server.js
@@ -0,0 +1,23 @@
+'use strict';
+
+//setup express
+//import express from 'express';
+const express = require('express');
+let app = express();
+app.set('port', process.env.PORT || 3001);
+app.use(express.static('./public'));
+
+//Main route, redirect to the react portion of the app (because of the bundle.js file)
+app.get('/', (request, response) =>{
+    response.sendFile('./public/index.html');
+});
+
+
+
+
+
+
+//start server
+app.listen(app.get('port'),() => {
+    console.log('Server started at: http://localhost/' + app.get('port')+'/');
+});
