e533765e2babc35afc8c3a9502ab81cdb6d4f391
MikeH <mike123.henry@gmail.com>
2016-09-25 22:16:10 -0500

    added react-bootstrap, refactored NavBar.js and added handleLogin functionality

--- app/components/children/NavBar.js
+++ app/components/children/NavBar.js
@@ -1,48 +1,48 @@
 import React from 'react';
+import { Navbar, Nav, NavItem, Input, Button, ButtonToolbar } from 'react-bootstrap'
 
 export default React.createClass({
     getInitialState: function(){
         return { isLoggedIn: false}
     },
 
+    handleLogin: function(){
+        if (this.state.isLoggedIn){
+            this.setState({ isLoggedIn: false})
+        } else {
+            this.setState({isLoggedIn: true})
+        }
+    },
+
     render(){
-        let something;
+        let loginFlag, signInFlag;
+        let floatRRight = {
+            float: 'right',
+            margin: 10
+            }
+
+
         if(this.state.isLoggedIn){
-            something = (<li><a href="/api/getEmpColl">logged in</a></li>)
+            loginFlag = ( <Button bsSize="small" bsStyle="danger" type="submit"  onClick={this.handleLogin} >Log Out</Button>)
         } else{
-            something = (<li><a href="/api/getEmpColl">not logged in</a></li>)
+            loginFlag = ( <Button bsSize="small" bsStyle="success" type="submit" onClick={this.handleLogin} >Log In</Button>)
+            signInFlag = ( <Button bsSize="small" bsStyle="info" type="submit" >Sign Up</Button>)
         }
         return (
             <div className="container">
                 {/*add navbar*/}
-                <nav className="navbar navbar-default" role="navigation">
-                    <div className="container-fluid">
-                        <div className="navbar-header">
-                            {/*add the mobile responsive collapsable nav bar 'hamburger' */}
-                            {/*<button></button>*/}
-                            <button type="button" className="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-                                {/*sr-only is screen rider only*/}
-                                <span className="sr-only">Toggler Navigaton</span>
-                                {/*each icon-bar is a layer in the "hamgerger*/}
-                                <span className="icon-bar"></span>
-                                <span className="icon-bar"></span>
-
-                                <span className="icon-bar"></span>
-                            </button>
-                            {/*add the company brand (when we get the logo put it here)*/}
-                            <a className="nav-brand" href="#">PGS TimeSheet</a>
-                        </div>
-                        {/*put the navbar stuff here -in desk mode they are the buttons -in mobile it the stuff in the hamberger */}
-                        <div className="collapse navbar-collapse navbar-ex1-collapse">
-                            <ul className="nav navbar-nar navbar-right">
-                                { something }
-                                <li><a href="/api/getEmpColl">/api/getEmpColl</a></li>
-                                <li><a href="/api/getEmpColl">/api/getEmpColl</a></li>
-                                <li><a href="/api/getEmpColl">/api/getEmpColl</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </nav>
+                <Navbar>
+                    <Navbar.Header>
+                        <Navbar.Brand >
+                            <a href="#">Pro Gate</a>
+                        </Navbar.Brand>
+                        <ButtonToolbar style={floatRRight}>
+                              {loginFlag}
+                              {signInFlag}
+                        </ButtonToolbar>
+                    </Navbar.Header>
+                </Navbar>
+                  <Button type="submit">Submit</Button>
                 {/*add page title*/}
             </div>
         )
